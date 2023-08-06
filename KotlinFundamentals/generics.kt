50be6a4e2b98fee95b959fc19b3924b442a41d80
Thierry <thierryalefh77@gmail.com>
2023-08-06 19:27:41 -0300

    Companion object and extensions

--- KotlinFundamentals/generics.kt
+++ KotlinFundamentals/generics.kt
@@ -1,3 +1,14 @@
+class Quiz {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+}
+
 data class Question<T>(
     val questionText: String,
     val answer: T,
@@ -8,13 +19,18 @@ enum class Difficulty {
     EASY, MEDIUM, HARD
 }
 
+val Quiz.StudentProgress.progressText: String
+    get() = "$answered of $total answered"
+
+fun Quiz.StudentProgress.printProgress() {
+    repeat(answered) { print('▓') }
+    repeat(total - answered) { print('▒') }
+}
+
+
 
 fun main() {
-    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
-    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
-    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
-
-    println(question1.toString())
-    println(question2.toString())
-    println(question3.toString())
+    println(Quiz.progressText)
+    println()
+    Quiz.printProgress()
 }
