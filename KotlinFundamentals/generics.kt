db0778bd40818fb79587a78dff89484d8263f999
Thierry <thierryalefh77@gmail.com>
2023-08-06 19:01:34 -0300

    Enum and data class

--- KotlinFundamentals/generics.kt
+++ KotlinFundamentals/generics.kt
@@ -1,16 +1,20 @@
-class Question<T>(
+data class Question<T>(
     val questionText: String,
     val answer: T,
-    val difficulty: String
-) {
+    val difficulty: Difficulty
+)
+
+enum class Difficulty {
+    EASY, MEDIUM, HARD
 }
 
-fun main() {
-    val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
-    val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
-    val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
 
-    println(question1.questionText + "\n" + question1.answer + "\n" + question1.difficulty + "\n")
-    println(question2.questionText + "\n" + question2.answer + "\n" + question2.difficulty + "\n")
-    println(question3.questionText + "\n" + question3.answer + "\n" + question3.difficulty + "\n")
+fun main() {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+    println(question1.toString())
+    println(question2.toString())
+    println(question3.toString())
 }
