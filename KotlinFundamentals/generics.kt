2ed620a69fcdffeb4ce3c82ef6d2d40453e97e98
Thierry <thierryalefh77@gmail.com>
2023-08-06 20:19:25 -0300

    Interfaces and scope functions

--- KotlinFundamentals/generics.kt
+++ KotlinFundamentals/generics.kt
@@ -1,4 +1,10 @@
-class Quiz {
+interface PrintableProgress {
+    val progressText: String
+    fun printProgress()
+}
+
+
+class Quiz:PrintableProgress {
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
@@ -6,6 +12,37 @@ class Quiz {
     companion object StudentProgress {
         var total: Int = 10
         var answered: Int = 3
+    }
+
+    override val progressText: String
+        get() = "$answered of $total answered"
+
+    override fun printProgress() {
+        repeat(answered) { print('▓') }
+        repeat(total - answered) { print('▒') }
+        println()
+        println(progressText)
+    }
+
+    fun printQuiz() {
+        question1.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+        question2.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+        question3.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
     }
 }
 
@@ -19,18 +56,14 @@ enum class Difficulty {
     EASY, MEDIUM, HARD
 }
 
-val Quiz.StudentProgress.progressText: String
-    get() = "$answered of $total answered"
-
-fun Quiz.StudentProgress.printProgress() {
-    repeat(answered) { print('▓') }
-    repeat(total - answered) { print('▒') }
-}
-
-
 
 fun main() {
-    println(Quiz.progressText)
+    val quiz = Quiz()
+    quiz.printQuiz()
+
     println()
-    Quiz.printProgress()
+
+    quiz.apply {
+        printQuiz()
+    }
 }
